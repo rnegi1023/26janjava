8ee800d797ece3968b02bde3ba4221cb568f82b4
RAHUL NEGI <rnegi1795@gmail.com>
2026-01-26 13:36:29 +0530

    Update year in Republic Day message from 1950 to 1995

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("26th January is celebrated as Republic Day in India.");
-        System.out.println("It marks the date on which the Constitution of India came into effect in 1950.");
+        System.out.println("It marks the date on which the Constitution of India came into effect in 1995.");
         System.out.println("On this day, a grand parade is held in the capital city, New Delhi, showcasing India's cultural diversity and military strength.");
         System.out.println("The President of India hoists the national flag and delivers a speech to the nation.");
         System.out.println("Republic Day is a national holiday and is celebrated with great enthusiasm across the country.");
